7c9f1651a67691be74e76e023c465a6683c166ed
Juan <diegonovoa001@gmail.com>
2026-08-23 12:00:01 -0500

    feat: dinamizar submenus en el sidebar de admin

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -87,35 +87,29 @@
 
 
                         @isset($link['submenu'])
-
+                            {{-- Cambio por IA: Menú desplegable dinámico vinculado al arreglo $link['submenu'] --}}
                             <button type="button"
                                 class="flex items-center w-full justify-between px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group"
-                                aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                                <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312" />
-                                </svg>
-                                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
-                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                aria-controls="dropdown-{{ $loop->index }}" data-collapse-toggle="dropdown-{{ $loop->index }}">
+                                <span class="w-6 h-6 inline-flex justify-center items-center text-gray-500">
+                                    <i class="{{ $link['icon'] }}"></i>
+                                </span>
+                                <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-gray-500">{{ $link['name'] }}</span>
+                                <svg class="w-5 h-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     fill="none" viewBox="0 0 24 24">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="m19 9-7 7-7-7" />
                                 </svg>
                             </button>
-                            <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                                <li>
-                                    <a href="#"
-                                        class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Products</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Billing</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="pl-10 flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">Invoice</a>
-                                </li>
+                            <ul id="dropdown-{{ $loop->index }}" class="hidden py-2 space-y-2">
+                                @foreach ($link['submenu'] as $item)
+                                    <li>
+                                        <a href="{{ $item['href'] }}"
+                                            class="pl-11 flex items-center px-2 py-1.5 text-sm text-gray-500 rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group {{ $item['active'] ? 'bg-gray-100' : '' }}">
+                                            {{ $item['name'] }}
+                                        </a>
+                                    </li>
+                                @endforeach
                             </ul>
                         @else
                             <a href="{{ $link['href'] }}"
